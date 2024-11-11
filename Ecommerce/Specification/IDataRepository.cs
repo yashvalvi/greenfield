9df909a0bf43636fa646db30f453117d0bb3259e
Yash Valvi <yash.valvi@simplifyhealthcare.com>
2024-11-11 10:46:48 +0000

    collectiom

--- Ecommerce/Specification/IDataRepository.cs
+++ Ecommerce/Specification/IDataRepository.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using POCOLib
+using POCOLib;
 namespace Specification
 {
-    public interface IDataRepository
+    public interface IDataRepository<T>
     {
-        bool Serialize(string filename, List<product>products);
-        List<product> Deserialize(string filename); 
+        bool Serialize(string filename, List<T>products);
+        List<T> Deserialize(string filename); 
     }
 }
 }
